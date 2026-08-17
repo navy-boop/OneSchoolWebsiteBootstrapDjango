08ea9b00d67f57a0e8f0102a7958bfd8fe956cbb
navy <keqianxuetang@gmail.com>
2026-08-17 07:45:39 -0230

    update fifth

--- OneSchoolWebsiteBootstrapDjango/settings.py
+++ OneSchoolWebsiteBootstrapDjango/settings.py
@@ -91,10 +91,11 @@ USE_I18N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 MAILERS = {
